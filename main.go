38b6eba3c9ee993f4d6a926158790d0252059567
Shichida Masatsugu <shichida.masatsugu@moneyforward.co.jp>
2022-02-19 16:15:58 +0900

    ✨8000ポートのアクセスとdbの接続

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var e = createMux()
 
 func main() {
-	fmt.Println("hoge")
+	port := os.Getenv("PORT")
+	connectDB()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, hoge!")
+	})
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CSRF())
+
+	return e
+}
+
+func connectDB() {
+	dsn := os.Getenv("DSN")
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	log.Println("db connection succeeded")
 }
